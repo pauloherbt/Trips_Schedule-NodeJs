87cbc7549ebab1b4c4b00ccc21443ef4b6640d62
peaga <pauloherbert@alu.ufc.br>
2024-07-17 21:08:00 -0300

    add: eslint & code format

--- src/routes/trips.ts
+++ src/routes/trips.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { unknown, z } from "zod"
+import { z } from "zod"
 import { prisma } from "../lib/prisma";
-import { resolve } from "path";
 
 export async function trips_routes(app: FastifyInstance) {
 
@@ -20,33 +19,33 @@ export async function trips_routes(app: FastifyInstance) {
         return prisma.trip.findMany();
     })
 
-    app.get('/:id',async (req,res)=>{
-        const id = z.object({id:z.string().uuid()}).parse(req.params);
-        return prisma.trip.findUniqueOrThrow({where:id}).catch((err)=>res.callNotFound())
+    app.get('/:id', async (req, res) => {
+        const id = z.object({ id: z.string().uuid() }).parse(req.params);
+        return prisma.trip.findUniqueOrThrow({ where: id }).catch(() => res.callNotFound())
     })
 
-    app.put('/:id',async (req,res)=>{
+    app.put('/:id', async (req, res) => {
 
         const id = z.object({
-            id:z.string().uuid()
+            id: z.string().uuid()
         }).parse(req.params)
 
-        const newTripData=z.object({
-            destination:z.optional(z.string()),
+        const newTripData = z.object({
+            destination: z.optional(z.string()),
             starts_at: z.optional(z.coerce.date()),
-            ends_at : z.optional(z.coerce.date()),
+            ends_at: z.optional(z.coerce.date()),
             is_confirmed: z.optional(z.coerce.boolean())
         }).parse(req.body)
-        
-        return prisma.trip.update({where:id,data:newTripData}).catch((err)=>res.callNotFound());
+
+        return prisma.trip.update({ where: id, data: newTripData }).catch(() => res.callNotFound());
     })
 
-    app.delete('/:id',async (req,res)=>{
+    app.delete('/:id', async (req, res) => {
 
         const id = z.object({
-            id:z.string().uuid()
+            id: z.string().uuid()
         }).parse(req.params)
 
-        return prisma.trip.delete({where:id}).catch((error)=>res.callNotFound());
+        return prisma.trip.delete({ where: id }).catch(() => res.callNotFound());
     })
 }
